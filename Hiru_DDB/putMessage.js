b22bcb824f22ee7076f0be3aa0a751ace92553dd
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 10:43:21 +0530

    put message

---
+++ Hiru_DDB/putMessage.js
@@ -0,0 +1,24 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	
+	var message ={};
+	message.id = event.id;
+	message.name = event.name;
+	
+	ddb.put({
+		TableName: 'HiruTest',
+		Item: { 'ID': message.id, 'Name': message.name }
+	}, function (err, data) {
+		if (err) {
+			console.log('error : ',err);
+			callback(null, err);
+		} else {
+			console.log('success : ',data);
+			callback(null, data);
+		}
+	});
+
+
+	//callback(null, 'Successfully executed');
+}
