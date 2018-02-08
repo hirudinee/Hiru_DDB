b17cd79bb49a8590d6e6b0db612513256dc96d8a
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-08 13:24:13 +0530

    query

--- Hiru_DDB/putMessage.js
+++ Hiru_DDB/putMessage.js
@@ -1,21 +1,25 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-	
-	var message ={};
+
+	var message = {};
 	message.id = event.id;
 	message.name = event.name;
 	message.year = event.year;
-	
+
 	ddb.put({
 		TableName: 'HiruTest',
-		Item: { 'ID': message.id, 'Name': message.name, 'Year':message.year }
+		Item: {
+			'ID': message.id,
+			'Name': message.name,
+			'Year': message.year
+		}
 	}, function (err, data) {
 		if (err) {
-			console.log('error : ',err);
+			console.log('error : ', err);
 			callback(null, err);
 		} else {
-			console.log('success : ',data);
+			console.log('success : ', data);
 			callback(null, data);
 		}
 	});
